199792e218f50acd91a1f05191a613feb78597b4
Dulneth Mohottala <144834521+Dulnethrwm@users.noreply.github.com>
2025-03-06 23:03:00 +0000

    blahhhhhh

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/olympians')
+def olympians():
+    return render_template('olympians.html')
+
+# Add other routes for different pages
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ olympians.js
@@ -0,0 +1,9 @@
+// Function to toggle visibility of the additional content
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
